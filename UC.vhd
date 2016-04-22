781aafe351bb40eebf08682679ea9794f0949ebe
osoriodiego <dieferosorio@utp.edu.co>
2016-04-22 10:19:45 -0500

    Control Unit -sintaxis corregida-

--- UC.vhd
+++ UC.vhd
@@ -1,6 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+-----------------------------------------------------------------------------------------------------------------------
+
 
 entity UC is
     Port ( op : in  STD_LOGIC_VECTOR (1 downto 0);
@@ -8,19 +9,19 @@ entity UC is
            ALUOP : out  STD_LOGIC_VECTOR (5 downto 0));
 end UC;
 
-
 architecture Behavioral of UC is
 begin
 	process(op,op3)
 	  begin
 			if(op = "01")then ----------------------------------------op 01 CALL
 				ALUOP <= "111111";
+			end if;
 			
 
 			if(op = "00")then ----------------------------------------op 00 BRANCH --SETHI
-				case op3 is
-					--
-				end case;
+				--case op3 is
+				--	---
+				--end case;
 			end if;			
 
 
@@ -36,30 +37,34 @@ begin
 					when "000001" => -- AND
 						ALUOP <= "000010";
 
-					when "000010" => -- OR
-						ALUOP <= "000011";
-
 					when "000101" => -- ANDN
 						ALUOP <= "000100";
-
+						
+					when "000010" => -- OR
+						ALUOP <= "000011";
+						
 					when "000110" => -- ORN
 						ALUOP <= "000101";
-
+	
 					when "000011" => -- XOR
 						ALUOP <= "000110";
 
 					when "000111" => -- XORN
 						ALUOP <= "000111";
+					
+					when others => 
+						null;
+						
+				end case;
 			end if;
 
 
 			if(op = "11")then ------------------------------------------- op 11 MEMORY INSTRUCTIONS
-				case op3 is
-					--
-				end case;
+				--case op3 is
+				--	--
+				--end case;
 			end if;
 
 	end process;
 end Behavioral;
-#procesador-monociclo
-# procesador-monociclo
+
