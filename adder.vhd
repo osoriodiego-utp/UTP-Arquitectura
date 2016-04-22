796bb599f7238eb4e3d46279bba4f5ddbd8c08dc
osoriodiego <dieferosorio@utp.edu.co>
2016-04-22 09:10:34 -0500

    Adder -sintaxis corregida-

---
+++ adder.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_unsigned.all;
+-----------------------------------------------------------------------------------------------------------------------
+
+
+entity adder is
+    Port ( entradaADDER : in  STD_LOGIC_VECTOR (32 downto 0);
+           salidaADDER : out  STD_LOGIC_VECTOR (32 downto 0));
+end adder;
+
+architecture Behavioral of adder is
+begin
+	salidaADDER <= entradaADDER + '1';
+
+end Behavioral;
+
