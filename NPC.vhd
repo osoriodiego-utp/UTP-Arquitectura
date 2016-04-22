4947bb43de1fbe5ee13f1698f76a14406a4268bb
osoriodiego <dieferosorio@utp.edu.co>
2016-04-22 09:08:39 -0500

    Next Program Counter - sintaxis corregida-

---
+++ NPC.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+-----------------------------------------------------------------------------------------------------------------------
+
+
+entity NPC is
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           entradaNPC : in  STD_LOGIC_VECTOR (31 downto 0);
+           salidaNPC : out  STD_LOGIC_VECTOR (31 downto 0));
+end NPC;
+
+architecture Behavioral of NPC is
+
+begin
+process (clk,reset,entradaNPC)
+	begin
+		if (rising_edge(clk))then
+			if (reset='1') then
+				salidaNPC <= (others=> '0'); --x"00000000";
+			else
+				salidaNPC <= entradaNPC;
+			end if;
+		end if;
+end process;
+
+end Behavioral;
