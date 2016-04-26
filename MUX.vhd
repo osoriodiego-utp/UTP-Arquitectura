12fd3ed54113ee82d79d6a1763eebf0c32ca9536
osoriodiego <dieferosorio@utp.edu.co>
2016-04-25 19:43:30 -0500

    MUX - sintaxis corregida-

---
+++ MUX.vhd
@@ -0,0 +1,25 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_unsigned.all;
+-----------------------------------------------------------------------------------------------------------------------
+
+entity MUX is
+    Port ( i : in  STD_LOGIC;
+			  rs2s : in  STD_LOGIC_VECTOR (31 downto 0);
+			  imm32 : in  STD_LOGIC_VECTOR (31 downto 0);
+           salidaMUX : out  STD_LOGIC_VECTOR (31 downto 0));
+end MUX;
+
+architecture Behavioral of MUX is
+begin
+	process(i, rs2s, imm32)
+		begin
+			if (i = '1') then
+				salidaMUX <= imm32;
+				else
+				salidaMUX <= rs2s;
+			end if;
+end process;
+end Behavioral;
+
+
